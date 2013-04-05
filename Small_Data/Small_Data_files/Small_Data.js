c3634fb8da445d14ef2dede1d0d5068a63b3fc4f
yaelelmatad <yael.elmatad@gmail.com>
2013-04-05 12:51:47 -0400

    fixedFormat

--- Small_Data/Small_Data_files/Small_Data.js
+++ Small_Data/Small_Data_files/Small_Data.js
@@ -6,6 +6,6 @@ function photocastSubscribe()
 {photocastHelper("http://yaelelmatad.github.com/Small_Data/Small_Data/rss.xml");}
 function onPageLoad()
 {loadMozillaCSS('Small_Data_files/Small_DataMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');detectBrowser();fixAllIEPNGs('../Media/transparent.gif');Widget.onload();fixupAllIEPNGBGs();performPostEffectsFixups()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');detectBrowser();Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('../Media/transparent.gif');performPostEffectsFixups()}
 function onPageUnload()
 {Widget.onunload();}
